ac652592a257d641dfdec3dccc00c0a223bdc76a
lgarin211 <lagustinus211@gmail.com>
2020-11-03 07:45:47 +0700

    lain

---
+++ ads.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 03, 2020 at 01:44 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ads`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asess`
+--
+-- Error reading structure for table ads.asess: #1932 - Table 'ads.asess' doesn't exist in engine
+-- Error reading data for table ads.asess: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`asess`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+-- Error reading structure for table ads.barang: #1932 - Table 'ads.barang' doesn't exist in engine
+-- Error reading data for table ads.barang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`barang`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chart`
+--
+-- Error reading structure for table ads.chart: #1932 - Table 'ads.chart' doesn't exist in engine
+-- Error reading data for table ads.chart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`chart`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `db`
+--
+-- Error reading structure for table ads.db: #1932 - Table 'ads.db' doesn't exist in engine
+-- Error reading data for table ads.db: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`db`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `has`
+--
+-- Error reading structure for table ads.has: #1932 - Table 'ads.has' doesn't exist in engine
+-- Error reading data for table ads.has: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`has`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `text-assis`
+--
+-- Error reading structure for table ads.text-assis: #1932 - Table 'ads.text-assis' doesn't exist in engine
+-- Error reading data for table ads.text-assis: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`text-assis`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+-- Error reading structure for table ads.transaksi: #1932 - Table 'ads.transaksi' doesn't exist in engine
+-- Error reading data for table ads.transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`transaksi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table ads.user: #1932 - Table 'ads.user' doesn't exist in engine
+-- Error reading data for table ads.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`user`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_access_menu`
+--
+-- Error reading structure for table ads.user_access_menu: #1932 - Table 'ads.user_access_menu' doesn't exist in engine
+-- Error reading data for table ads.user_access_menu: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`user_access_menu`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+-- Error reading structure for table ads.user_menu: #1932 - Table 'ads.user_menu' doesn't exist in engine
+-- Error reading data for table ads.user_menu: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`user_menu`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+-- Error reading structure for table ads.user_role: #1932 - Table 'ads.user_role' doesn't exist in engine
+-- Error reading data for table ads.user_role: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`user_role`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sub_menu`
+--
+-- Error reading structure for table ads.user_sub_menu: #1932 - Table 'ads.user_sub_menu' doesn't exist in engine
+-- Error reading data for table ads.user_sub_menu: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`user_sub_menu`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_token`
+--
+-- Error reading structure for table ads.user_token: #1932 - Table 'ads.user_token' doesn't exist in engine
+-- Error reading data for table ads.user_token: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`user_token`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `view_artikel`
+--
+-- Error reading structure for table ads.view_artikel: #1932 - Table 'ads.view_artikel' doesn't exist in engine
+-- Error reading data for table ads.view_artikel: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`view_artikel`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `view_galeri`
+--
+-- Error reading structure for table ads.view_galeri: #1932 - Table 'ads.view_galeri' doesn't exist in engine
+-- Error reading data for table ads.view_galeri: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`view_galeri`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `view_kategori`
+--
+-- Error reading structure for table ads.view_kategori: #1932 - Table 'ads.view_kategori' doesn't exist in engine
+-- Error reading data for table ads.view_kategori: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `ads`.`view_kategori`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
